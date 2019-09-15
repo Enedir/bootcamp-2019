de9a055f0a5e1709674a3b7e5ef3aba2b2b0b682
enedir <guimaraes.junior94@gmail.com>
2019-09-15 14:27:10 -0300

    Adicionado a funcionalidade de agendamento

--- GoBarber/src/routes.js
+++ GoBarber/src/routes.js
@@ -5,6 +5,8 @@ import muterConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -17,6 +19,11 @@ routes.post('/session', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', update.single('file'), FileController.store);
 
