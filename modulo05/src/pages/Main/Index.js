aa439235abf4f938b341f8bf1c1224d8d5369b11
enedir <guimaraes.junior94@gmail.com>
2019-10-11 01:12:29 -0300

    Criação das rotas e do estilos

---
+++ modulo05/src/pages/Main/Index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+// export default function Main() {
+//   return (
+//     <Title error>
+//       Main
+//       <small>menor</small>
+//     </Title>
+//   );
+// }
+
+export default function Main() {
+  return <Title>Hello Word!</Title>;
+}
---
+++ modulo05/src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Repository from './pages/Repository/Index';
+import Main from './pages/Main/Index';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/repository" component={Repository} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
